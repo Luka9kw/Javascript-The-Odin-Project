dfa1bc8f3fc44bfa518dc781c97062cdc67132ed
Luka9kw <luka9kw@hotmail.com>
2024-01-18 10:42:55 -0300

    Exercicio de manipulação da DOM finalizado

--- foundations/dom-manipulation-and-events/script.js
+++ foundations/dom-manipulation-and-events/script.js
@@ -1,4 +1,4 @@
-const div = document.createElement('div'); // Não cria o elemento na DOM, nó na memória para manipular estilo, classes, id etc.
+const div = document.createElement('div'); // Não cria o elemento na DOM, só na memória para manipular estilo, classes, id etc.
 
 div.style.color = 'blue'; // Adiciona uma regra de estilo
 div.style.cssText = 'color: blue; background: white;' // Adiciona várias regras de estilo
