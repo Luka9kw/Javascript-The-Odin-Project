6e179c86e3cefd907f0f88218338d244c102f263
Luka9kw <luka9kw@hotmail.com>
2024-01-26 11:18:48 -0300

    elementos criados na dom

--- foundations/dom-manipulation-and-events/events/assignment/basic-dom/script.js
+++ foundations/dom-manipulation-and-events/events/assignment/basic-dom/script.js
@@ -1,4 +1,11 @@
 const link = document.querySelector("a");
+const paragraph = document.createElement("p");
+const section = document.querySelector("section");
 
-link.textContent = 'Mozilla Developer Network';
-link.href = 'https://developer.mozilla.org';
+link.textContent = "Mozilla Developer Network";
+paragraph.textContent = "We hope you enjoyed the ride.";
+
+link.href = "https://developer.mozilla.org";
+
+section.appendChild(paragraph);
+
