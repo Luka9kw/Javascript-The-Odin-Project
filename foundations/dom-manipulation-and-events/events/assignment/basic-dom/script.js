f5cb47c473ae46e388c522f7179d8a2aaca03030
Luka9kw <luka9kw@hotmail.com>
2024-01-30 09:39:56 -0300

    teste

--- foundations/dom-manipulation-and-events/events/assignment/basic-dom/script.js
+++ foundations/dom-manipulation-and-events/events/assignment/basic-dom/script.js
@@ -8,5 +8,9 @@ paragraph.textContent = "We hope you enjoyed the ride.";
 link.href = "https://developer.mozilla.org";
 
 section.appendChild(paragraph);
+section.appendChild(link); // Desce o link para o final da página onde está loclizado o node section.
 
-section.appendChild(link); // Desce o link par o final da página onde está loclizado o node section.
+
+// Criando um text node
+const text = document.createTextNode(" - the premier source for web development knowledge.")
+paragraph.appendChild(text)
